79f3ba0d0dbfc1a8be50574cdd3466e3e6bebc97
LukeSaward <55454721+LukeSaward1@users.noreply.github.com>
2022-01-28 00:35:28 +0000

    Upload project files

---
+++ DIFM-ModLoaderGUI/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+
+namespace DIFM_ModLoaderGUI
+{
+    public partial class Form1 : Form
+    {
+        public string downloadPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                downloadPath = folderDlg.SelectedPath + @"\Do It For Me V1.0.1_Data\Managed\Assembly-CSharp.dll";
+                textBox1.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                if(textBox2.Text.Contains("Latest") || textBox2.Text.Contains("latest"))
+                {
+                    client.DownloadFile("https://raw.githubusercontent.com/LukeSaward1/DiFM-Speedrun-Mod/main/Assembly-CSharp.dll", downloadPath);
+                }
+                else
+                {
+                    string actualURL = "https://github.com/LukeSaward1/DiFM-Speedrun-Mod/releases/download/v" + textBox2.Text + "/Assembly-CSharp.dll";
+                    client.DownloadFile(actualURL, downloadPath);
+                }
+                MessageBox.Show("The file has been successfully downloaded and is located in the 'Managed' folder in the 'Do It For Me V1.0.1_Data' folder under Assembly-CSharp.dll", "File successfully downloaded");
+            }
+        }
+    }
+}
---
+++ DIFM-ModLoaderGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DIFM-ModLoaderGUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DIFM-ModLoaderGUI")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c85f0b7-65f9-4032-b9c1-ca0538d5bbd5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
